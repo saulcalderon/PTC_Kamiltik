ba0b7f8aeba0db4bd4c192fc3c70c9588fc80712
maroartu <maro.artu2@gmail.com>
2020-04-30 22:55:46 -0600

    Arreglo responsive de quienes somos

--- views/commerce/quienesSomos.php
+++ views/commerce/quienesSomos.php
@@ -19,72 +19,95 @@ Commerce::headerTemplate();
         <h1>Visión</h1>
         <p>"Ser el Coffee Shop salvadoreño preferido a nivel internacional por su calidad de café gourmet y su servicio personalizado."</p>
     </div>
-    <div class="col s6 center">
+    <div class="col s6 center"> 
         <h1>Misión</h1>
         <p>"Posicionarnos como la mejor cadena de café Gourmet más admirada y representativa de los salvadoreños a nivel global."</p>
     </div>
 </div>
 
 <!-- Prinicipios, objetivos y valores -->
-<div class="row QS">
-
-    <div class="col s12 center">
+<div class="row">
+    <!-- Principios -->
+    <div class="col l12 s12 m12 center">
         <h1>Principios</h1>
     </div>
-    <div class="col s6 pv">
-        <ul>
-            <li>• Energía positiva.</li>
-            <li>• Espíritu de familia.</li>
-            <li>• Optimización de recursos.</li>
-            <li>• Exclencia en ejecuciones.</li>
-            <li>• Confiar en el trabajo en equipo.</li>
-            <li>• Reconocimientos en los logros alcanzados.</li>
-            <li>• Responsabilidad de las labores asignadas.</li>
-            <li>• Superar las expectativas de nuestros clientes.</li>
-            <li>• Enseñar y apoyar la cultura de nuestras baristas.</li>
-        </ul>
+    <div class="col l6 s12 m12">
+    <div class="col l2 s2 m2"></div>
+        <div class="col l8 s8 m8">
+            <img src="../../resources/img/commerce/imagen.png" class="responsive-img">
+        </div>
+    <div class="col l2 s2 m2"></div>
     </div>
-    <div class="col s6 pv">
-        <img src="../../resources/img/commerce/imagen.png" class="responsive-img left">
+    <div class="col l6 s12 m12 center">
+        <div class="col l2 s2 m2"></div>
+        <div class="col l8 s8 m8">
+            <ul class="quienessomos">
+                <li>• Energía positiva.</li>
+                <li>• Espíritu de familia.</li>
+                <li>• Optimización de recursos.</li>
+                <li>• Excelencia en ejecuciones.</li>
+                <li>• Confiar en el trabajo en equipo.</li>
+                <li>• Reconocimiento de los logros alcanzados.</li>
+                <li>• Responsabilidad de las labores asignadas.</li>
+                <li>• Superar las expectativas de nuestros clientes.</li>
+                <li>• Enseñar y apoyar la cultura de nuestras baristas.</li>
+            </ul>
+        </div>
+        <div class="col l2 s2 m2"></div>
     </div>
-</div>
-
-<div class="row QS">
-    <div class="col s12 center">
+    <!-- Objetivos -->
+    <div class="col l12 s12 m12 center">
         <h1>Objetivos</h1>
     </div>
-    <div class="col s6 o">
-        <img src="../../resources/img/commerce/imagen.png" class="responsive-img right">
+    <div class="col l6 s12 m12 right">
+    <div class="col l2 s2 m2"></div>
+        <div class="col l8 s8 m8">
+            <img src="../../resources/img/commerce/imagen.png" class="responsive-img">
+        </div>
+    <div class="col l2 s2 m2"></div>
     </div>
-    <div class="col s6 o">
-        <ul>
-            <li>• Garantizacion el liderazgo de The Coffee Cup en sus mercados actuales y potenciales.</li>
-            <li>• Generar una experiencia satisfactoria a nuestros clientes a través de productos de calidad, ambiente y servicio personalizado.</li>
-            <li>• Innovar y consolidar una cultura de barismo en el personal femenino para hacer de ellas, mujeres emprendedoras orientadas al crecimiento individual y profesional.</li>
-        </ul>
+    <div class="col l6 s12 m12 center">
+        <div class="col l2 s2 m2"></div>
+        <div class="col l8 s8 m8">
+            <ul class="quienessomos">
+                <li>• Garantizacion el liderazgo de The Coffee Cup en sus mercados actuales y potenciales.</li>
+                <li>• Generar una experiencia satisfactoria a nuestros clientes a través de productos de calidad, ambiente y servicio personalizado.</li>
+                <li>• Innovar y consolidar una cultura de barismo en el personal femenino para hacer de ellas, mujeres emprendedoras orientadas al crecimiento individual y profesional.</li>
+            </ul>
+        </div>
+        <div class="col l2 s2 m2"></div>
     </div>
-</div>
-
-<div class="row QS">
-    <div class="col s12 center">
+    <!-- Valores -->
+    <div class="col l12 s12 m12 center">
         <h1>Valores</h1>
     </div>
-    <div class="col s6 pv">
-        <ul>
-            <li>• Honestidad</li>
-            <li>• Lealtad</li>
-            <li>• Prudencia</li>
-            <li>• Respeto</li>
-            <li>• Cortesía</li>
-            <li>• Responsabilidad</li>
-            <li>• Perseveracia</li>
-            <li>• Compañerismo</li>
-        </ul>
+    <div class="col l6 s12 m12">
+    <div class="col l2 s2 m2"></div>
+        <div class="col l8 s8 m8">
+            <img src="../../resources/img/commerce/imagen.png" class="responsive-img">
+        </div>
+    <div class="col l2 s2 m2"></div>
     </div>
-    <div class="col s6 pv">
-        <img src="../../resources/img/commerce/imagen.png" class="responsive-img left">
+    <div class="col l6 s12 m12 center">
+        <div class="col l2 s2 m2"></div>
+        <div class="col l8 s8 m8">
+            <ul class="quienessomos">
+                <li>• Honestidad</li>
+                <li>• Lealtad</li>
+                <li>• Prudencia</li>
+                <li>• Respeto</li>
+                <li>• Cortesía</li>
+                <li>• Responsabilidad</li>
+                <li>• Perseverancia</li>
+                <li>• Compañerismo</li>
+            </ul>
+        </div>
+        <div class="col l2 s2 m2"></div>
     </div>
+
 </div>
+    
+    
 <?php
 Commerce::FooterTemplate();
 ?>
